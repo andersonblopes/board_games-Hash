e00889937c3c8352b6cf3b1d9cd29c131f47b1e5
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-10-28 10:21:08 -0300

    Create test to check if game is over

--- Project/src/main/java/com/lopes/Game.java
+++ Project/src/main/java/com/lopes/Game.java
@@ -2,4 +2,8 @@ package com.lopes;
 
 public class Game {
 
+	public boolean endUp() {
+		return false;
+	}
+
 }
--- Project/src/test/java/com/lopes/test/GameTest.java
+++ Project/src/test/java/com/lopes/test/GameTest.java
@@ -1,5 +1,17 @@
 package com.lopes.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.lopes.Game;
+
 public class GameTest {
+
+	@Test
+	public void createGame() {
+		Game game = new Game();
+
+		Assert.assertFalse("The game is over?", game.endUp());
+	}
 
 }

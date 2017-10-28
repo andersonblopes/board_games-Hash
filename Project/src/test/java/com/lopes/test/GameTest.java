889271e37df8729df9c20067e8998238332cf6b4
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-10-28 10:54:11 -0300

    Rearrange tests

--- Project/src/test/java/com/lopes/test/GameTest.java
+++ Project/src/test/java/com/lopes/test/GameTest.java
@@ -1,32 +1,33 @@
 package com.lopes.test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.lopes.Game;
 
 public class GameTest {
 
+	private Game game;
+
+	@Before
+	public void Init() {
+		game = new Game();
+	}
+
 	@Test
 	public void createGame() {
-		Game game = new Game();
-
 		Assert.assertFalse("The game is over?", game.endUp());
 	}
 
 	@Test
 	public void setFirstPlayer() {
-		Game game = new Game();
-
 		game.setPlayerMarkX(true);
-
 		Assert.assertTrue(game.isPlayerMarkX());
 	}
 
 	@Test
 	public void setFirstPlayerAgain() {
-		Game game = new Game();
-
 		game.setPlayerMarkX(true);
 		game.setPlayerMarkX(false);
 
